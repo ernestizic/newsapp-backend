459c9be7839b5f8d2f976d41cb6de5ce05b96442
ernestizic <ieifeanyichukwu@gmail.com>
2021-10-11 21:50:22 +0100

    index file added

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const articles = require('./routes/api/articles'); //import articles route
 const users = require('./routes/api/users'); //import users route
+const ejs = require('ejs');
 const cors = require('cors');
 const fs = require('fs')
 const path = require('path');
-
 
 
 const connectDB = require('./config/db');
@@ -17,6 +17,8 @@ connectDB();
 
 const app = express();
 
+//EJS
+app.set('view engine', 'ejs');
 
 
 ////////////////////////////////////////// Middleware ///////////////////////////////////////////
@@ -29,13 +31,23 @@ app.use(cors());
 
 
 
+
 //////////////////////////////////////// END OF MIDDLEWARE ///////////////////////////////////////
-
-
 
 // Public folder
 app.use(express.static(path.join(__dirname, 'public', 'images')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//////////////////ejs routes
+app.get('/', (req, res)=> {
+    res.render("index")
+});
+
+app.get('/admin', (req, res)=> {
+    res.render('admin')
+});
+
+// ejs routes ends here
 
 app.use('/api/v1', articles)
 app.use('/api', users)
