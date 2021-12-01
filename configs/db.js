285f162d066bcc2eff15c08cf8ed98062087366d
ernestizic <ieifeanyichukwu@gmail.com>
2021-12-01 20:13:26 +0100

    fix: mongo atlas for storage

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost/newsapp", {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
