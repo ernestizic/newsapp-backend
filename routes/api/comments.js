578902fe86c5011a4a0e08ba078f6a6550ed98cc
ernestizic <ieifeanyichukwu@gmail.com>
2021-12-01 19:54:27 +0100

    feat: comment

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -10,7 +10,7 @@ const auth = require("../../middleware/auth");
 // @route /api/article/id
 router.post("/article/:id", auth, async (req, res) => {
   try {
-    // check prior to findById call to see if id is a valid ObjectId
+    // make check prior to findById call to see if id is a valid ObjectId
     if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
       const article = await Article.findById(req.params.id);
       if (article) {
